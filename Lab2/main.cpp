912f306862a05d88083509cfb1ac4d4d7ed5e6e0
Sergey Galuzov <seregasuper2000@gmail.com>
2020-11-12 17:46:37 +0200

    almost done

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,155 +1,208 @@
 #include <iostream>
-#include <vector>
+#include <deque>
+#include <map>
 #include <cmath>
+#include <cstring>
 
 using namespace std;
-const int PAGE_SIZE = 1024;
-const int MIN_BLOCK_SIZE = 16;
 
-class PageDescriptor;
-size_t calculateBlockSize(size_t size)
-{
-    size_t blockSize = MIN_BLOCK_SIZE;
-    while (blockSize < size)
-    {
-        blockSize *= 2;
-    }
-    return blockSize;
-}
-
-size_t calculatePageAmountInBlockSize(size_t size)
-{
-    size_t pageAmount = 1;
-    while (pageAmount * PAGE_SIZE < size)
-    {
-        pageAmount++;
-    }
-    return pageAmount;
-}
-
-
-enum State
-{
-    Free,
-    DividedOnBlocks,
-    BLockPart
-};
-
-class Block
-{
-public:
-    int size;
-    byte* data;
-    bool isFree;
-    Block(size_t size)
-    {
-        this->size = size;
-        isFree = true;
-    }
-};
-
-class PageDescriptor
-{
-public:
-    State pageState;
-    int freeBlocksNumber;
-    int pageAmount;
-    vector <Block*> blocks;
-    PageDescriptor()
-    {
-        pageState = State::Free;
-    }
-};
-
-void* createMultiPageBlock(vector<PageDescriptor*> &descriptors, int index, int pageAmount)
-{
-    for (auto i = index; i < pageAmount; i++)
-    {
-        descriptors[i]->blocks.push_back(new Block(PAGE_SIZE));
-        descriptors[i]->blocks[0]->isFree = false;
-        descriptors[i]->pageState = State::BLockPart;
-    }
-    descriptors[index]->pageAmount = pageAmount;
-    return &descriptors[index]->blocks[0];
-}
+size_t const PageSize = 1024;
+size_t const MinBlockSize = 16;
 
 class PageAllocator
 {
+
+private:
+    enum class PageState
+    {
+        Free,
+        DividedIntoBlocks,
+        MultiplePageBlock,
+    };
+    struct PageDescriptor
+    {
+        PageState state;
+        byte* pageStart;
+        byte* firstFreeBlock;
+        size_t freeBlocksAmount;
+        size_t blockSize;
+        size_t pageAmount;
+    };
+
+    deque <PageDescriptor*> descriptors;
+    byte* memory;
+    size_t pageAmountInAllocator;
+
+
+    size_t CalculateBlockSize(size_t size)
+    {
+        if (size <= MinBlockSize) return MinBlockSize;
+        size_t blockSize = MinBlockSize;
+        while (blockSize < size)
+        {
+            blockSize *= 2;
+        }
+        return blockSize;
+    }
+
+    byte* DivideFreePageIntoBlocks(PageDescriptor* descriptor, size_t blockSize)
+    {
+        descriptor->blockSize = blockSize;
+        descriptor->state = PageState::DividedIntoBlocks;
+        auto blockAddr = descriptor->firstFreeBlock;
+        descriptor->firstFreeBlock += blockSize;
+        descriptor->freeBlocksAmount = PageSize / blockSize - 1;
+        return blockAddr;
+    }
+    byte* CreateMultiPageBlock(deque<PageDescriptor*> selectedDescriptors, size_t pageAmount)
+    {
+        selectedDescriptors[0]->pageAmount = pageAmount;
+        for (auto &descriptor : selectedDescriptors)
+        {
+            descriptor->state = PageState::MultiplePageBlock;
+            descriptor->freeBlocksAmount = 0;
+            descriptor->firstFreeBlock = nullptr;
+            descriptor->blockSize = PageSize;
+        }
+        return selectedDescriptors[0]->pageStart;
+    }
+
 public:
-    vector <PageDescriptor*> descriptorsList;
     PageAllocator(size_t size)
     {
-        for (auto i = 0; i < size / PAGE_SIZE; i++)
+        auto allocatorSize = size - size % PageSize;
+        pageAmountInAllocator = allocatorSize / PageSize;
+        memory = new byte[allocatorSize];
+        descriptors.resize(pageAmountInAllocator);
+        for (int i = 0; i < pageAmountInAllocator; i++)
         {
-            descriptorsList.push_back(new PageDescriptor());
+            byte* pageAddr = memory + i * PageSize;
+            PageDescriptor* descriptor;
+            descriptor->state = PageState::Free;
+            descriptor->pageStart = pageAddr + i * PageSize;
+            descriptor->firstFreeBlock = pageAddr;
+            descriptors[i] = descriptor;
         }
     }
+
     void* mem_alloc(size_t size)
     {
-        size_t blockSize;
-        if (size <= PAGE_SIZE / 2)
+        if (size <= PageSize / 2)
         {
-            blockSize = calculateBlockSize(size);
-            for (auto i = 0; i < descriptorsList.size(); i++)
+            auto blockSize = CalculateBlockSize(size);
+            for (auto &descriptor : descriptors)
             {
-                if (descriptorsList[i]->pageState == State::Free)
+                if (descriptor->state == PageState::DividedIntoBlocks and descriptor->freeBlocksAmount > 0)
                 {
-                    for (int j = 0; j < PAGE_SIZE / blockSize; j++)
+                    auto blockAddr = descriptor->firstFreeBlock;
+                    descriptor->freeBlocksAmount--;
+                    if (descriptor->freeBlocksAmount == 0)
                     {
-                        descriptorsList[i]->blocks.push_back(new Block(blockSize));
-                        descriptorsList[i]->freeBlocksNumber++;
+                        descriptor->firstFreeBlock = nullptr;
                     }
-                    descriptorsList[i]->blocks[0]->isFree = false;
-                    descriptorsList[i]->freeBlocksNumber--;
-                    descriptorsList[i]->pageState = State::DividedOnBlocks;
-                    return &descriptorsList[i]->blocks[0];
-                }
-                else
-                {
-                    if (descriptorsList[i]->pageState == State::DividedOnBlocks)
-                    {
-                        for (int j = 0; j < descriptorsList[i]->blocks.size(); j++)
-                        {
-                            if (descriptorsList[i]->blocks[j]->isFree)
-                            {
-                                descriptorsList[i]->blocks[j]->isFree = false;
-                                descriptorsList[i]->freeBlocksNumber--;
-                                return descriptorsList[i]->blocks[j];
-                            }
-                        }
-                    }
-
+                    else descriptor->firstFreeBlock += blockSize;
+                    return blockAddr;
                 }
             }
-            return nullptr;
+
+            auto firstFreePageDescriptorIterator = find_if(descriptors.begin(), descriptors.end(), [](PageDescriptor* descriptor)
+            {
+                return descriptor->state == PageState::Free;
+            });
+
+            if (firstFreePageDescriptorIterator == descriptors.end())
+            {
+                return nullptr;
+            }
+            else
+            {
+                return DivideFreePageIntoBlocks(*firstFreePageDescriptorIterator, blockSize);
+            }
+
         }
         else
         {
-            size_t pageAmount = calculatePageAmountInBlockSize(size);
-            for (int i = 0; i < descriptorsList.size() + 1 - pageAmount; i++)
+            size_t pageAmount = ceil((double)size / PageSize);
+            deque<PageDescriptor*> selectedDescriptors;
+            for(int i = 0; i < descriptors.size(); i++)
             {
-                bool flag = true;
-                for (int j = i; j < i + pageAmount; j++)
+                if (descriptors[i]->state == PageState::Free)
                 {
-                    if (descriptorsList[j]->pageState != State::Free)
-                    {
-                        flag = false;
-                        break;
-                    }
+                    selectedDescriptors.push_back(descriptors[i]);
                 }
-                if (flag)
+                else selectedDescriptors.clear();
+
+                if (selectedDescriptors.size() == pageAmount)
                 {
-                    return createMultiPageBlock(descriptorsList, i, pageAmount);
+                    return CreateMultiPageBlock(selectedDescriptors, pageAmount);
                 }
             }
             return nullptr;
         }
+    }
+
+    void mem_free(void* addr)
+    {
+        auto pageDescriptorIterator = find_if(descriptors.begin(), descriptors.end(), [addr](PageDescriptor* descriptor)
+        {
+            return (descriptor->pageStart <= addr and addr < descriptor->pageStart + PageSize);
+        });
+        PageDescriptor* descriptor = *pageDescriptorIterator;
+
+        if (descriptor->state == PageState::DividedIntoBlocks)
+        {
+            descriptor->firstFreeBlock -= descriptor->blockSize;
+            descriptor->freeBlocksAmount--;
+            if (descriptor->freeBlocksAmount == 0) descriptor->state = PageState::Free;
+        }
+        else
+        {
+            auto pageAmount = descriptor->pageAmount;
+            deque<PageDescriptor*> multiPageBlock(pageDescriptorIterator, pageDescriptorIterator + pageAmount);
+            for (auto &pageDescriptor : multiPageBlock)
+            {
+                pageDescriptor->pageAmount = 0;
+                pageDescriptor->state = PageState::Free;
+
+            }
+        }
+
+    }
+
+    void* mem_realloc(void* addr, size_t size)
+    {
+        mem_free(addr);
+        return mem_alloc(size);
+    }
+
+    void mem_dump()
+    {
+        cout << "Page amount: " << pageAmountInAllocator << endl;
+        cout << "Page size: " << PageSize << endl;
+        for (int i = 0; i < pageAmountInAllocator; i++)
+        {
+            cout << "Page " << i + 1 << " | ";
+            cout << "Address: " << descriptors[i]->pageStart << " | ";
+            auto state = descriptors[i]->state == PageState::Free ? "Free" :
+                         descriptors[i]->state == PageState::DividedIntoBlocks ? "DividedIntoBlocks" : "MultiplePageBlock";
+            cout << "State: " << state << endl;
+
+            /*auto page = memory + i * PageSize;
+            auto header = headers[page];
+            auto state = header.state == PageState::Free ? "Free" :
+                         header.state == PageState::Divided ? "Divided" : "Multiple";
+            auto classSize = header.state == PageState::Divided ? " | Class size: " + to_string(header.blockSize) : "";
+            auto freePlace = header.state == PageState::Divided ? " | Free blocks: " + to_string(header.blocks) : "";
+            cout << "Page " << page << " " << state << classSize << freePlace << endl;*/
+        }
+        cout << endl;
     }
 };
 
 int main()
 {
-    PageAllocator a(1024);
-    a.mem_alloc(960);
+    auto allocator = PageAllocator(4096);
+    auto loc1 = allocator.mem_alloc(102);
+    allocator.mem_dump();
     return 0;
 }
